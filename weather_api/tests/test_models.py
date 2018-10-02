52bb6509d31792abd5e9ee1655e7eeb4a3d03fda
realslimdjd <jamdel72@gmail.com>
2018-10-01 20:35:53 -0500

    tests?

--- weather_api/tests/test_models.py
+++ weather_api/tests/test_models.py
@@ -11,21 +11,21 @@ class WeatherAPITests(SimpleTestCase):
             'celsius' : 12})
         
         temp = weather.get_noaa(44, 33)
-        self.assertEquals(temp, expected_reponse)
+        self.assertEqual(temp, expected_reponse)
 
     def test_weather_dot_com(self):
         expected_reponse = (True, {'fahrenheit' : 37,
             'celsius' : None})
 
         temp = weather.get_weather_dot_com(33.3, 44.4)
-        self.assertEquals(temp, expected_reponse)
+        self.assertEqual(temp, expected_reponse)
 
     def get_accuweather(self):
         expected_reponse = (True, {'fahrenheit' : 55,
             'celsius' : 12})
 
         temp = get_accuweather(44, 33)
-        self.assertEquals(temp, expected_reponse)
+        self.assertEqual(temp, expected_reponse)
 
 
 # This could be expanded to include all combinations of services
@@ -38,54 +38,48 @@ class WeatherHandlersTests(SimpleTestCase):
         expected_reponse = (True, 49)
 
         results = get_avg_temp(44, 33)
-        self.assertEquals(results, expected_reponse)
+        self.assertEqual(results, expected_reponse)
 
     def get_avg_temp_bad_filter(self):
         expected_reponse = (False, 'An invalid service was requested. The available services are: noaa, weather.com, accuweather')
 
         results = get_avg_temp(44, 33)
-        self.assertEquals(results, expected_reponse)
+        self.assertEqual(results, expected_reponse)
 
     def get_avg_temp_noaa(self):
         expected_reponse = (True, 55)
 
         results = get_avg_temp(44, 33, 'noaa')
-        self.assertEquals(results, expected_reponse)
+        self.assertEqual(results, expected_reponse)
 
     def get_avg_temp_weatherdotcom(self):
         expected_reponse = (True, 37)
 
         results = get_avg_temp(33.3, 44.4, 'weather.com')
-        self.assertEquals(results, expected_reponse)
+        self.assertEqual(results, expected_reponse)
 
     def get_avg_temp_accuweather(self):
         expected_reponse = (True, 55)
 
         results = get_avg_temp(44, 33, 'accuweather')
-        self.assertEquals(results, expected_reponse)
+        self.assertEqual(results, expected_reponse)
 
     def get_avg_temp_allfilters(self):
         expected_reponse = (True, 49)
 
         results = get_avg_temp(44, 33, ['noaa', 'weather.com', 'accuweather'])
-        self.assertEquals(results, expected_reponse)
+        self.assertEqual(results, expected_reponse)
 
     def get_weather_good_request(self):
-        expected_reponse = JsonResponse({'Data' : {'Temperature' : {'Fahrenheit' : 33}}})
+        expected_reponse = JsonResponse({'Data' : {'Temperature' : {'Fahrenheit' : 37}}})
 
         request = self.factory.get('/weather?latitude=44&longitude=33&filters=weather.com')
         results = get_weather(request)
-        self.assertEquals(results, expected_reponse)
+        self.assertEqual(results, expected_reponse)
 
     def get_weather_no_latlong(self):
         expected_reponse =  JsonResponse({'Error' :'Please include latitude and longitude'})
 
         request = self.factory.get('/weather?filters=weather.com')
         results = get_weather(request)
-        self.assertEquals(results, request)
-
-
-
-
-
-
+        self.assertEqual(results, request)
